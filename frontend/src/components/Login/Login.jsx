25734181dc94bdcafa176b49759eab8814a71994
Sharad <sharadd19@gmail.com>
2024-07-25 18:23:10 +0100

    added back links and styling

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,12 +1,14 @@
-import { Form, useActionData, useNavigate } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
 import styles from "../Login/Login.module.css";
 export default function Login() {
-
+  const navigate = useNavigate();
   return (
     <>
       <h1>Login</h1>
       <div className={styles.wrapper}>
         <Form method="post">
+          <button className={styles.back} onClick={() => navigate(-1)}>Back </button>
+
           <div className={styles.username}>
             <label htmlFor="username">Username:</label>
             <input type="text" name="username" />
@@ -19,6 +21,10 @@ export default function Login() {
             {navigation.state === "submitting" ? "Logging in..." : "Login"}
           </button>
         </Form>
+        <div className={styles.signup}>
+          or
+          <Link to="/signup">Create an account!</Link>
+        </div>
       </div>
     </>
   );
--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom"
+import styles from "../SignUp/SignUp.module.css"
 export default function SignUp() {
-
+const navigate = useNavigate();
     return (
         <>
         <h1>Create your account!</h1>
+        <button className={styles.back} onClick={() => navigate(-1)}>
+      Go Back
+    </button>
         </>
     )
-
 }
+
